95a435386ea077de05aaca6ff3d130c4369ea695
Jelle de Bie <jelledebie2005@gmail.com>
2024-10-04 10:07:25 +0200

    add Database migrations, Controllers, Models

---
+++ database/migrations/2024_10_04_072640_create_tasks_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->foreignId('project_id')->constrained()->onDelete('cascade');
+            $table->enum('status', ['done', 'ready', 'in progress', 'todo'])->default('todo');
+            $table->dateTime('deadline')->nullable();
+            $table->dateTime('completed_at')->nullable();
+            $table->tinyInteger('deleted')->default(0);
+            $table->timestamps();
+        });
+    }
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
