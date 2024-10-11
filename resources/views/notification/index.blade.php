feca146c767e2f5e6f5796ea9928b90305e2f2fa
Rowen Bieshaar <rowen.04@hotmail.com>
2024-10-11 10:15:25 +0200

    set yield

--- resources/views/notification/index.blade.php
+++ resources/views/notification/index.blade.php
@@ -1,40 +1,40 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container mt-2">
-    <div class="row">
-        <div class="col-lg-12 margin-tb">
-            <div class="pull-left">
-                <h2>Notificaties</h2>
+    <div class="container mt-2">
+        <div class="row">
+            <div class="col-lg-12 margin-tb">
+                <div class="pull-left">
+                    <h2>Notificaties</h2>
+                </div>
             </div>
         </div>
+        <div class="row">
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th>Project Id</th>
+                        <th>Issue No.</th>
+                        <th>Status</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($Notifications as $Notification)
+                        <tr>
+                            <td>{{ $Notification->project_id }}</td>
+                            <td>{{ $Notification->issue_id }}</td>
+                            <td>{{ $Notification->status }}</td>
+                            <td>
+                                <form action="{{ route('notifications.destroy', $Notification->id) }}" method="Post">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-danger">Delete</button>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
     </div>
-    <div  class="row">
-        <table class="table table-bordered">
-            <thead>
-                <tr>
-                    <th>Issue No.</th>
-                    <th>Status</th>
-                    <th>Project Id</th>
-                </tr>
-            </thead>
-            <tbody>
-            @foreach ($Notifications as $Notification)
-                <tr>
-                    <td>{{ $Notification->movie }}</td>
-                    <td>{{ $Notification->content }}</td>
-                    <td>{{ $Notification->content }}</td>
-                    <td>
-                        <form action="{{ route('notifications.destroy', $Notification->id) }}" method="Post">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" class="btn btn-danger">Delete</button>
-                        </form>
-                    </td>
-                </tr>
-            @endforeach
-            </tbody>
-        </table>
-    </div>
-</div>
 @endsection
