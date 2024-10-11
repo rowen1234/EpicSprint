434c2ba5227cc23bf0b6db530da1f4158a0c70c9
Rowen Bieshaar <rowen.04@hotmail.com>
2024-10-11 09:35:13 +0200

    extend notification model

--- app/Models/notification.php
+++ app/Models/notification.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class notification extends Model
 {
     use HasFactory;
+
+    public function user()
+    {
+        $this->belongsTo(User::class, 'user_id');
+    }
+    public function project()
+    {
+        $this->belongsTo(project::class, 'project_id');
+    }
+    public function task()
+    {
+        $this->belongsTo(task::class, 'task_id');
+    }
 }
