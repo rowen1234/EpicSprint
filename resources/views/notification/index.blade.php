8888c17865b39dda423805296021ca6c3b92b425
Rowen Bieshaar <rowen.04@hotmail.com>
2024-10-18 09:42:55 +0200

    add additional columns to notification page

--- resources/views/notification/index.blade.php
+++ resources/views/notification/index.blade.php
@@ -14,8 +14,10 @@
                 <thead class="table-primary">
                     <tr>
                         <th>Project Id</th>
-                        <th>Issue No.</th>
+                        <th>Task No.</th>
                         <th>Status</th>
+                        <th>Gemaakt op</th>
+                        <th>Deadline</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -24,6 +26,8 @@
                             <td>{{ $notification->project_id }}</td>
                             <td>{{ $notification->issue_id }}</td>
                             <td>{{ $notification->status }}</td>
+                            <td>{{ $notification->created_at }}</td>
+                            <td>{{ $notification->deadline }}</td>
                             <td>
                                 <form action="{{ route('notifications.destroy', $notification->id) }}" method="Post">
                                     @csrf
